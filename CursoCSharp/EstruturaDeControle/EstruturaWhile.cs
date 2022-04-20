d6d2cb3e350a6888ff4b091cc4d1b1c26337c2c8
Alfredo Gomes Pereira <71193893+alfredo1995@users.noreply.github.com>
2022-04-20 16:52:16 -0300

    Update EstruturaWhile.cs

--- CursoCSharp/EstruturaDeControle/EstruturaWhile.cs
+++ CursoCSharp/EstruturaDeControle/EstruturaWhile.cs
@@ -10,27 +10,33 @@ namespace CursoCSharp.EstruturaDeControle
     {
         public static void Executar()
         {
+            //
             int palpite = 0;
+            // criar um valor aleatorio emcima da class Random
             Random random = new Random();
 
+            // emcima de random e gerado um valor aleatorio
             int numeroSecreto = random.Next(1, 16);
             bool numeroEncontrado = false;
             int tentativaRestante = 5;
             int tentativas  = 0;
 
+            //enquanto as tentativas for maior q 0 e não for encontrado o numero
             while(tentativaRestante > 0 && !numeroEncontrado){
                 Console.WriteLine("Insira seu palpite: ");
                 string entrada = Console.ReadLine();
 
                 int.TryParse(entrada, out palpite);
 
+                //incrementar e decrementar a quantidade de tentativas
                 tentativas++;
                 tentativaRestante--;
 
                 if(numeroSecreto == palpite)
                 {
                     numeroEncontrado = true;
-                    Console.WriteLine("{0}", tentativas);
+                    //interpolar o valor mostrando o resultado das tentativas
+                    Console.WriteLine(" Numero encontrado em {0} tentativas", tentativas);
                 }else if(palpite > numeroSecreto)
                 {
                     numeroEncontrado = true;
